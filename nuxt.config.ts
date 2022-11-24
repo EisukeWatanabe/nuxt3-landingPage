0fa14b82b6a76cd3eb5b5cc1cfff6f0201f0e008
渡邉英資 <awatanabe@fminc.xsrv.jp>
2022-11-24 22:10:17 +0900

    fix nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
+    modules: ['@nuxtjs/tailwindcss']
 })
